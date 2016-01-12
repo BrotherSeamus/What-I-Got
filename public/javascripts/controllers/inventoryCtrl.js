8fb2c5cac01deefbbdfd95b1fe93d2d10f01fa82
BrotherSeamus <jamespkirwin@gmail.com>
2016-01-11 18:04:38 -0600

    can create, get, delete collectios, categories.

--- public/javascripts/controllers/inventoryCtrl.js
+++ public/javascripts/controllers/inventoryCtrl.js
@@ -18,87 +18,153 @@ app.controller('inventoryCtrl', ['authService', '$scope', '$location', '$http',
 			method: 'get'
 		}).then(function(res) {
 			$scope.collections = res.data;
+		}).then(function() {
+			$http({
+				url: 'inventory/category/' + $scope.collections[0]._id,
+				method: 'get'
+			}).then(function(res) {
+				$scope.categories = res.data;
+				$scope.currentCollection = $scope.collections[0]._id
+			}).then(function() {
+				$http({
+					url: 'inventory/item/' + $scope.categories[0]._id,
+					method: 'get'
+				}).then(function(res) {
+					$scope.items = res.data;
+					$scope.currentCategory = $scope.categories[0]._id
+				})
+			})
 		});
 
 /*---------------Collections--------------*/
 		// Get call to server for Collections list
 		$scope.getCollections = function(){
 
-			console.log($scope.user.id);
-
 			$http({
 				url: '/inventory/collection/' + $scope.user.id,
 				method: 'get'
 			}).then(function(res) {
 				$scope.collections = res.data;
-			});
-
+			})
 		};
 
 		// Post call to server to create New Collection
-		this.createCollection = function() {
+		$scope.createCollection = function() {
+
+			$http({
+				url: '/inventory/collection/' + $scope.user.id,
+				method: 'post',
+				data: {name: $scope.newCollection}
+			}).then(function() {
+				$scope.getCollections()
+			});
+
+			$scope.newCollection="";
 
 		};
 
 		// Update Collection
-		this.updateCollection = function() {
+		$scope.updateCollection = function() {
 
 		};
 
 		// Delete Collection
-		this.deleteCollection = function(){
+		$scope.deleteCollection = function(collId) {
 
+			if(confirm('All Categories and Items associated with this Collection will also be deleted. ' +
+					'\n\nDo you wish to continue?')) {
+				$http({
+					url: 'inventory/collection/' + collId,
+					method: 'delete'
+				}).then(function() {
+					$scope.getCollections();
+				})
+			}
 		};
 
 /*---------------Categories---------------*/
 		// Get call to server for Category list
 		$scope.getCategories = function(collId) {
 
-			console.log('fuck this');
+			$scope.currentCollection= collId;
 
 			$http({
 				url: 'inventory/category/' + collId,
 				method: 'get'
 			}).then(function(res) {
-				$scope.categories = res.data;
-				console.log(res.data);
+				$scope.categories= res.data;
+			}).then(function() {
+				if($scope.categories[0] != undefined) {
+					$scope.getItems($scope.categories[0]._id);
+				}else{
+					$scope.items = [];
+				}
 			})
 
 		};
 
 		// Post call to server to create New Category
-		this.createCategory = function() {
+		$scope.createCategory = function() {
+
+			$http({
+				url: '/inventory/category/' + $scope.currentCollection,
+				method: 'post',
+				data: {name: $scope.newCategory}
+			}).then(function() {
+				$scope.getCategories($scope.currentCollection)
+			});
+
+			$scope.newCategory="";
 
 		};
 
 		// Update Category
-		this.updateCategory= function() {
+		$scope.updateCategory= function() {
 
 		};
 
 		// Delete Category
-		this.deleteCategory = function(){
+		$scope.deleteCategory = function(catId){
+
+			if(confirm('All Items associated with this Category will also be deleted. ' +
+					'\n\nDo you wish to continue?')) {
+				$http({
+					url: 'inventory/category/' + catId,
+					method: 'delete'
+				}).then(function() {
+					$scope.getCategories($scope.currentCollection);
+				})
+			}
 
 		};
 
 /*---------------Items---------------*/
 		// Get call to server for Items list
-		this.getItems = function() {
+		$scope.getItems = function(catId) {
+
+			$scope.currentCategory= catId;
+
+			$http({
+				url: 'inventory/item/' + catId,
+				method: 'get'
+			}).then(function(res) {
+				$scope.items = res.data;
+			})
 
 		};
 
 		// Post call to server to create New Item
-		this.createItem = function() {
+		$scope.createItem = function() {
 
 		};
 
 		// Update Item
-		this.updateItem = function() {
+		$scope.updateItem = function() {
 
 		};
 
 		// Delete Item
-		this.deleteItem = function(){
+		$scope.deleteItem = function(){
 
 		};
 	}]);
